03bf57363f252825ae63dee0a73528c5f0a012db
Gustavo Bretas <gustavobretas@gmail.com>
2022-01-24 23:26:37 -0500

    Activity 3.01: Implement the Book Details View

--- AD130/bookr/bookr/urls.py
+++ AD130/bookr/bookr/urls.py
@@ -20,5 +20,7 @@ import reviews.views
 
 urlpatterns = {
     path('admin/', admin.site.urls),
+    path('', reviews.views.index),
+    path('book-search/', reviews.views.book_search, name='book_search'),
     path('', include('reviews.urls'))
 }
