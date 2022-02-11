1100f46335b270168be556c67125b7cf6f92a23d
Gustavo Bretas <gustavobretas@gmail.com>
2022-02-10 23:47:33 -0500

    Exercise 8.04: File Uploads with a Django Form

--- AD130/media_project/media_example/views.py
+++ AD130/media_project/media_example/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
+from .forms import UploadForm
 
 
 def media_example(request):
     if request.method == 'POST':
-        save_path = os.path.join(settings.MEDIA_ROOT, request.FILES["file_upload"].name)
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
 
-        with open(save_path, "wb") as output_file:
-            for chunk in request.FILES["file_upload"].chunks():
-                output_file.write(chunk)
+            with open(save_path, "wb") as output_file:
+                for chunk in form.cleaned_data["file_upload"].chunks():
+                    output_file.write(chunk)
+    else:
+        form = UploadForm()
 
-    return render(request, "media-example.html")
+    return render(request, "media-example.html", {"form": form})
