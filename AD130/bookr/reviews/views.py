8f4570add299cce9b6b4190ca80ba4fbd6d360b3
Gustavo Bretas <gustavobretas@gmail.com>
2022-02-13 07:58:59 -0500

    Activity 7.01: Styling and Integrating the Publisher Form

--- AD130/bookr/reviews/views.py
+++ AD130/bookr/reviews/views.py
@@ -91,4 +91,4 @@ def publisher_edit(request, pk=None):
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(request, "form-example.html", {"method": request.method, "form": form})
+    return render(request, "reviews/instance-form.html", {"method": request.method, "form": form})
